c03922e3fbe6a38299690379457f206108e561ed
shrestha-milan <76213964+shrestha-milan@users.noreply.github.com>
2021-04-29 19:14:10 +0545

    code updated

--- static/js/time.js
+++ static/js/time.js
@@ -1,5 +1,5 @@
     // Set the date we're counting down to
-    var countDownDate = new Date("April 25, 2021 15:37:25").getTime();
+    var countDownDate = new Date("May 3, 2021 15:37:25").getTime();
     
     // Update the count down every 1 second
     var x = setInterval(function() {
